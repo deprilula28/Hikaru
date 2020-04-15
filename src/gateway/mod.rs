5f3b65431e8765e8727bec176b84e64cd8dd94ec
April <april@pleasejustend.me>
2020-04-15 10:05:58 -0400

    a

--- src/gateway/mod.rs
+++ src/gateway/mod.rs
@@ -1,8 +1,5 @@
-#[macro_use]
 pub mod shardconnection;
-#[macro_use]
 pub mod gatewayopcode;
-#[macro_use]
 pub mod gatewayclosecode;
-#[macro_use]
 pub mod gatewaypayloadhandler;
+
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,13 @@ use ansi_term::Color;
 use crate::util::error::Error;
 use crate::gateway::shardconnection::Shard;
 
-#[macro_use]
-pub mod rest;
-#[macro_use]
-pub mod util;
-#[macro_use]
-pub mod gateway;
+mod rest;
+#[macro_use] mod util;
+mod gateway;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    ansi_term::enable_ansi_support()?;
+    ansi_term::enable_an ()?;
     let mut shard = Shard::new(&format!("Bot {}", env::var("token").expect("Token not provided")), 0, 1)?;
 
     if let Err(e) = shard.shard_loop() {
